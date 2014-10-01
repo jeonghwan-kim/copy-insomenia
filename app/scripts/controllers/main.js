f4bac5dcd1f11b535b1b463e88e1d1a82778ab6c
jeonghwan <ej88ej@gmail.com>
2014-10-01 17:17:18 +0900

    done

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the ngInsomeniaApp
  */
 angular.module('ngInsomeniaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $log) {
+
+      angular.element(document).scroll(function() {
+        var val = angular.element(this).scrollTop();
+        $log.debug('scroll: ', val);
+        if (val > 100) {
+          $scope.isTop = false;
+        } else {
+          $scope.isTop = true;
+        }
+      });
+
   });
